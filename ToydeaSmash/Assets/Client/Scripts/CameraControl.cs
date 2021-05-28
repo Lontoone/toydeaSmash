c30213da4d37f03845a57b3f50444ac19b18fc01
cingjia <news2000tw@gmail.com>
2021-05-28 19:38:07 +0900

    add readme

---
+++ ToydeaSmash/Assets/Client/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public int maxSize = 10, minSize = 5;
+    Camera camera;
+
+    public IEnumerator Start()
+    {
+        camera = Camera.main;
+
+        //wait for all player
+        yield return new WaitForFixedUpdate();
+
+        //scale between all players
+        
+    }
+
+    public void FixedUpdate()
+    {
+        //get camera bounds
+
+        //calculate distance between players
+
+        //move to the center of players
+
+    }
+}
