48b3a6ec84cbd0af3584bea82c07ddcb0f3ddd7e
Sean <sean0990461@gmail.com>
2021-06-06 21:09:32 +0900

    character selection button amend

---
+++ ToydeaSmash/Assets/Animtion_Playsound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Animtion_Playsound_Function : MonoBehaviour
+{
+
+
+    public AudioSource audioSource;
+
+
+    void Update()
+    {
+        audioSource.Play();
+    }
+}
