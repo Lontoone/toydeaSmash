ea679bb25992bf864e9dfebd3e139704d33cbc5c
cingjia <news2000tw@gmail.com>
2021-06-10 12:44:02 +0900

    Fix Online Panel. Fix Game Play Scene collider. Fix Start Lobby. Replace player slot varient

--- ToydeaSmash/Assets/Client/Scripts/Data/CustomPropertyCode.cs
+++ ToydeaSmash/Assets/Client/Scripts/Data/CustomPropertyCode.cs
@@ -11,9 +11,9 @@ public class CustomPropertyCode : MonoBehaviour
 
 
     public static readonly Color[] TEAMCOLORS = { Color.red, Color.green, Color.white, Color.blue };
-    public static readonly KeyCode[] JumpKeys = { KeyCode.Space, KeyCode.KeypadEnter };
-    public static readonly KeyCode[] DashKyes = { KeyCode.LeftShift, KeyCode.RightShift};
+    public static readonly KeyCode[] JumpKeys = { KeyCode.G, KeyCode.Period };
+    public static readonly KeyCode[] DashKyes = { KeyCode.LeftShift, KeyCode.Space};
     public static readonly KeyCode[] DuckKyes = { KeyCode.S, KeyCode.DownArrow};
-    public static readonly KeyCode[] AttackKyes = { KeyCode.Z, KeyCode.Keypad1 };
-    public static readonly KeyCode[] DefenseKyes = { KeyCode.X, KeyCode.Keypad2 };
+    public static readonly KeyCode[] AttackKyes = { KeyCode.F, KeyCode.Slash };
+    public static readonly KeyCode[] DefenseKyes = { KeyCode.H, KeyCode.Comma};
 }
