7c32bd7e910f273873b9ec9f8ea15aee265ab37a
cingjia <news2000tw@gmail.com>
2021-06-10 18:26:39 +0900

    Add resume countdown function

--- ToydeaSmash/Assets/Client/Scripts/Menu/PauseMenuControl.cs
+++ ToydeaSmash/Assets/Client/Scripts/Menu/PauseMenuControl.cs
@@ -1,18 +1,22 @@
 ﻿using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
 using UnityEngine;
-
 public class PauseMenuControl : MonoBehaviour
 {
     public GameObject panel;
+    public GameObject resumeConuntDownPanel;
+    public TextMeshProUGUI countDownText;
+    private Coroutine c_resumeCountDown;
     public void Quit()
     {
         if (PhotonNetwork.IsConnected)
         {
             PhotonNetwork.LeaveRoom();
         }
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");        
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
     }
     public void Start()
     {
@@ -24,9 +28,47 @@ public class PauseMenuControl : MonoBehaviour
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && c_resumeCountDown==null)
+        {
+            //panel.SetActive(true);
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        if (c_resumeCountDown != null)
+        {
+            return;
+        }
+        Time.timeScale = Mathf.Abs(1 - Time.timeScale);
+        if (Time.timeScale == 0)
         {
             panel.SetActive(true);
         }
+        else
+        {
+            panel.SetActive(false);
+            c_resumeCountDown = StartCoroutine(ResumeCountDownCoro());
+        }
+    }
+
+    private IEnumerator ResumeCountDownCoro()
+    {
+        WaitForSecondsRealtime _wait = new WaitForSecondsRealtime(1);
+        Time.timeScale = 0;
+        const int _TIME = 5;
+        int _counter = 0;
+        resumeConuntDownPanel.SetActive(true);
+        while (_counter < _TIME)
+        {
+            countDownText.text = (_TIME - _counter).ToString();
+            _counter++;
+            resumeConuntDownPanel.transform.DOPunchScale(Vector3.one , 0.5f).SetUpdate(true);
+            yield return _wait;
+        }
+        resumeConuntDownPanel.SetActive(false);
+        c_resumeCountDown = null;
+        Time.timeScale = 1;
     }
 }
