dd93a455d3aac066a46c12ef889165f116e26382
cingjia <news2000tw@gmail.com>
2021-06-10 13:34:01 +0900

    Fix boucncy range, Fix bouncy shape change error

--- ToydeaSmash/Assets/Client/Scripts/Level/BouncyFloor.cs
+++ ToydeaSmash/Assets/Client/Scripts/Level/BouncyFloor.cs
@@ -6,11 +6,29 @@ public class BouncyFloor : MonoBehaviour
 {
     public float force = 700;
     public float shackStrength = 20;
+    public float duration = 0.2f;
+    private Coroutine c_bouncyGap;
+    private WaitForSeconds _wait;
 
+    private void Start()
+    {
+        _wait = new WaitForSeconds(duration);
+    }
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>()?.AddForce(transform.up * force);
+        if (c_bouncyGap == null)
+        {
+            c_bouncyGap = StartCoroutine(BounceCoro(collision.gameObject));
+        }
+
+    }
+
+    IEnumerator BounceCoro(GameObject _obj)
+    {
+        _obj.GetComponent<Rigidbody2D>()?.AddForce(transform.up * force);
         transform.DOShakeScale(0.2f, shackStrength, 100);
+        yield return _wait;
+        c_bouncyGap = null;
     }
 
 }
