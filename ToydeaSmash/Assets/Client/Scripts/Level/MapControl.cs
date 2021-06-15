707073b96a86597b912e206d5b3b851cb4924ffb
cingjia <news2000tw@gmail.com>
2021-06-15 18:38:15 +0900

    loag game scene object

--- ToydeaSmash/Assets/Client/Scripts/Level/MapControl.cs
+++ ToydeaSmash/Assets/Client/Scripts/Level/MapControl.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapControl : MonoBehaviour
 {
     public static MapControl instance;
+    private const string MAP_DATA_PATH = "Prefab/Maps/";
     public void Awake()
     {
         if (instance == null)
@@ -15,12 +17,30 @@ public class MapControl : MonoBehaviour
         {
             Destroy(gameObject);
         }
+        LoadMapPrefab();
     }
-    //public Transform[]
     public Vector2 viewWorldCenter
     {
         get { return Camera.main.ViewportToWorldPoint(new Vector2(0.5f, 1)); }
     }
 
+    //load map
+    public void LoadMapPrefab()
+    {
+        //if online
+        if (PhotonNetwork.IsConnected && PhotonNetwork.IsMasterClient)
+        {
+            string _path = MAP_DATA_PATH + PhotonNetwork.MasterClient.CustomProperties[GameplaySettingControl.MAP_OPT];
+            GameObject _map = PhotonNetwork.Instantiate(_path, Vector2.zero, Quaternion.identity);
+        }
+        //if local
+        else
+        {
+            string _path = MAP_DATA_PATH + LocalRoomManager.instance.gamePlaySetting.GetValue<string>(GameplaySettingControl.MAP_OPT);
+            Debug.Log(_path);
+            GameObject _map = Instantiate( Resources.Load<GameObject>(_path),Vector2.zero,Quaternion.identity);
+            
+        }
+    }
 
 }
