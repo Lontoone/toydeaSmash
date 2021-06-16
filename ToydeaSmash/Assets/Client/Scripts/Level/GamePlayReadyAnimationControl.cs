1f52a31c8602bdd2beb8eaa675eae1cea6385070
cingjia <news2000tw@gmail.com>
2021-06-16 16:43:58 +0900

    introEffect

---
+++ ToydeaSmash/Assets/Client/Scripts/Level/GamePlayReadyAnimationControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayReadyAnimationControl : MonoBehaviour
+{
+    public float startDelay =0.5f;
+    public float lastDuration = 7;
+    private Animator animator;
+    private SpriteRenderer _sp;
+    private IEnumerator Start()
+    {
+        _sp = GetComponent<SpriteRenderer>();
+        _sp.enabled = false;
+        animator = GetComponent<Animator>();
+
+        yield return new WaitForSeconds(startDelay);
+        _sp.enabled = true;
+
+        animator.Play("ready animation");
+        yield return new WaitForSeconds(lastDuration);
+        Destroy(gameObject);
+    }
+
+}
